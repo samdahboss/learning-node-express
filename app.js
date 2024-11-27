9208fa0ea10ba524767e9610403dcc1e7dbc1a51
samdahboss <samuelolawumi65@gmail.com>
2024-11-27 11:30:22 +0100

    Add dotenv for environment variable management and enhance API routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import { config } from 'dotenv';
+
+config()
 const app = express()
-import {env} from 'node:process'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,7 +12,10 @@ app.get('/api/courses',(req, res) =>{
   res.send([1,2,3,4,5])
 });
 
+app.get('/api/courses/:id', (req, res)=>{
+  res.send(req.params)
+})
+
 //PORT
-const port = env.PORT || 3000;
-console.log(env.PORT);
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
