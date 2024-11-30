e9a7f0890802cf32768cf100ce9e549795b3f166
samdahboss <samuelolawumi65@gmail.com>
2024-11-30 07:48:08 +0100

    Add Joi validation for course creation and refactor API routes

--- app.js
+++ app.js
@@ -1,29 +1,53 @@
-import express from 'express'
-import { config } from 'dotenv';
+import Joi from "joi";
+import express from "express";
+import { config } from "dotenv";
 
-config()
-const app = express()
+config();
+const app = express();
 
+app.use(express.json());
 const courses = [
-  {id: 1, name: 'course1'},
-  {id: 2, name: 'course2'},
-  {id: 3, name: 'course3'}
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/courses',(req, res) =>{
-  res.send([1,2,3,4,5])
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3, 4, 5]);
 });
 
-app.get('/api/courses/:id', (req, res)=>{
-  const course = courses.find(c => c.id === parseInt(req.params.id))
-  if(!course) res.status(404).send('The course with the given ID was not found')
-  res.send(course)
-})
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+  res.send(course);
+});
 
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const result = schema.validate(req.body)
+  console.log(result);
+
+  if (result.error) {
+    //400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(courses);
+});
 //PORT
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, () => console.log(`Listening on port ${port}...`));
