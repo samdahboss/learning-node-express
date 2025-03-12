101b898e2734287b5a3249af7e89feef0da6ab47
samdahboss <samuelolawumi65@gmail.com>
2025-03-12 22:36:01 +0100

    Refactor async demo: improve callback structure for user data and repository retrieval

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,19 @@
 console.log("Before!");
-getUserData(1, (user) => {
-  //get user repositories
-  getUserRepositories(user.githubUsername, (repos) => {
-    console.log("User Repos", repos);
-  });
-});
+getUserData(1, getUserCallback);
 console.log("After!");
+
+// Callbacks
+function getUserCallback(user) {
+  getUserRepositories(user.githubUsername, getRepoCallback);
+}
+
+function getRepoCallback(repos) {
+  getCommits(repos[0], displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log("Commits", commits);
+}
 
 function getUserData(id, callback) {
   setTimeout(() => {
@@ -21,5 +29,12 @@ function getUserRepositories(username, callback) {
   setTimeout(() => {
     console.log("Fetching user Repositories............");
     callback([username + 1, username + 2, username + 3]);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log("Fetching commits............");
+    callback([repo + 1, repo + 2, repo + 3]);
   }, 2000);
 }
