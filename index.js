ef7dd3c2fe0d1ca368de32b17e1ee55fd9504508
samdahboss <samuelolawumi65@gmail.com>
2025-03-12 14:39:57 +0100

    Refactor hotel API: move functionality to separate router and consolidate main app setup

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from "express";
+
+//importing third party middlewares
+import helmet from "helmet";
+import morgan from "morgan";
+
+//importing joi for validation
+import Joi from "joi";
+
+//importing config
+import { config } from "dotenv";
+import AppConfig from "config";
+config(); //configuring the environment
+
+//importing debugger modules
+import debug from "debug";
+const startupDebugger = debug("app:startup");
+const dbDebugger = debug("app:db");
+
+// express app
+const app = express();
+app.use(express.json());
+app.use(express.static("public")); //built-in middleware to provide static assets e.g localhost:3000/image.png
+
+//adding templating engine
+app.set("view engine", "pug");
+app.set("views", "./views"); //default
+
+//importing routes
+import { hotelApiRouter } from "./routes/hotel.js";
+app.use("/api/hotels", hotelApiRouter);
+
+//getting the current environment
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get("env")}`);
+
+//middlewares
+app.use(helmet()); //middle ware to secure express applications
+
+if (app.get("env") === "development") {
+  //optionally use morgan middleware in development only
+  app.use(morgan("tiny")); // (morgan is for logging api requests to the server)
+  startupDebugger("Morgan Middleware Enabled....");
+}
+
+//db work
+dbDebugger("Connected to the database....");
+
+//configuration
+// console.log("Application Name: " + AppConfig.get("name"));
+// console.log("Mail Server: " + AppConfig.get("mail.host"));
+// console.log("Mail Password: " + AppConfig.get("mail.password"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Hotel API", message: "Welcome to Hotel API" });
+});
+
+const PORT = process.env.HOTEL_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Hotel API is running on port ${PORT}`);
+});
---
+++ routes/hotel.js
@@ -0,0 +1,82 @@
+import express from "express";
+
+
+export const hotelApiRouter = express.Router();
+
+// hotel array
+const hotels = [
+  { hotel_id: 1, hotel_name: "Marriot" },
+  { hotel_id: 2, hotel_name: "Hilton" },
+  { hotel_id: 3, hotel_name: "Sheraton" },
+  { hotel_id: 4, hotel_name: "Hyatt" },
+  { hotel_id: 5, hotel_name: "Radisson" },
+  { hotel_id: 6, hotel_name: "Ritz-Carlton" },
+  { hotel_id: 7, hotel_name: "Four Seasons" },
+  { hotel_id: 8, hotel_name: "Waldorf Astoria" },
+  { hotel_id: 9, hotel_name: "Intercontinental" },
+  { hotel_id: 10, hotel_name: "Fairmont" },
+];
+
+//function to validate hotel
+const validateHotel = (hotel) => {
+  const schema = Joi.object({
+    hotel_name: Joi.string().min(5).required(),
+  });
+
+  return schema.validate(hotel);
+};
+
+//api to get all hotel
+hotelApiRouter.get("/", (req, res) => {
+  res.send(hotels);
+});
+
+//api to get specific hotel
+hotelApiRouter.get("/:id", (req, res) => {
+  const hotel = hotels.find((h) => h.hotel_id === parseInt(req.params.id));
+
+  if (!hotel)
+    return res.status(404).send("An Hotel with the given id was not found");
+
+  res.send(hotel);
+});
+
+//api to add a hotel
+hotelApiRouter.post("/", (req, res) => {
+  const { error } = validateHotel(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const hotel = {
+    hotel_id: hotels.length + 1,
+    hotel_name: req.body.hotel_name,
+  };
+
+  hotels.push(hotel);
+  res.send(hotel);
+});
+
+//api to update a hotel
+hotelApiRouter.put("/:id", (req, res) => {
+  const hotel = hotels.find((h) => h.hotel_id === parseInt(req.params.id));
+
+  if (!hotel)
+    return res.status(404).send("An Hotel with the given id was not found");
+
+  const { error } = validateHotel(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  hotel.hotel_name = req.body.hotel_name;
+  res.send(hotel);
+});
+
+//api to delete a hotel
+hotelApiRouter.delete("/:id", (req, res) => {
+  const hotel = hotels.find((h) => h.hotel_id === parseInt(req.params.id));
+  if (!hotel)
+    return res.status(404).send("An Hotel with the given id was not found");
+
+  const index = hotels.indexOf(hotel);
+  hotels.splice(index, 1);
+
+  res.send(hotel);
+});
