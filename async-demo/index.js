f9055e8754f63b174c78e26b63fdbe8cb51089a4
samdahboss <samuelolawumi65@gmail.com>
2025-03-13 11:50:39 +0100

    Refactor async demo: add async/await approach and comment out promise chaining example

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,32 @@
 console.log("Before!");
 
-getUserData(1)
-  .then((user) => getUserRepositories(user.githubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
+// Promise Approach
+// getUserData(1)
+//   .then((user) => getUserRepositories(user.githubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
 
+// Callbacks Approach
+function getUserCallback(user) {
+  getUserRepositories(user.githubUsername, getRepoCallback);
+}
+
+function getRepoCallback(repos) {
+  getCommits(repos[0], displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log("Commits", commits);
+}
+
+//Async/Await Approach
+(async () => {
+  const user = await getUserData(1);
+  const repos = await getUserRepositories(user.githubUsername);
+  const commits = await getCommits(repos[0]);
+  console.log("Commits", commits);
+})();
 console.log("After!");
 
 function getUserData(id) {
