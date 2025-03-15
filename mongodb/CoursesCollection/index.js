c2ee96d90f7e978e5437d4ae46a36d4155643818
samdahboss <samuelolawumi65@gmail.com>
2025-03-15 15:26:21 +0100

    Add CoursesCollection with Mongoose schema and CRUD operations

---
+++ mongodb/CoursesCollection/index.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/LearningExpress")
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  tags: [String],
+  isPublished: Boolean,
+  author: String,
+  date: { type: Date, default: Date.now() },
+});
+
+const Course = new mongoose.model("courses", courseSchema);
+
+const AddCourse = async (name, price, tags, isPublished, author) => {
+  const course = new Course({
+    name,
+    price,
+    tags,
+    isPublished,
+    author,
+  });
+
+  const newCourse = await course.save();
+  console.log(newCourse);
+};
+
+// AddCourse("My Courses", 10000, ["best-seller"], true, "Olawumi Samuel");
+
+const getCourses = async () => {
+  const courses = await Course.find({ tags: "backend", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
+
+  console.log(courses);
+};
+
+getCourses();
